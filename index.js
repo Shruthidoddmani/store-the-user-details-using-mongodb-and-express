d0ec826efc6dd580fec6e8045c8c3f919329dfb2
Shruthidoddmani <shruthida2000@gmail.com>
2024-02-27 22:54:57 +0530

    storing input user details in mongodb

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://shruthida:QMlppRaIQ5crO1Bm@shruthi.ivb5ubm.mongodb.net/",
+);
+
+const User = mongoose.model("pichhiUser", {
+  name: String,
+  email: String,
+  password: String,
+});
+
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser) {
+    res.json({ message: "User already exists" });
+  }
+  const user = new User({ name: username, email: email, password: password });
+  user.save();
+  res.json({
+    msg: "User created successfully",
+  });
+});
+
+app.listen(3000);
